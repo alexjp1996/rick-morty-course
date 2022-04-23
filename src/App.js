510c54d676b805126acf72295256c55d88a70592
Marlon Jimenez <alex_jp1996@hotmail.com>
2022-04-23 00:46:08 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import imageRickMorty from './img/rick-morty.png'
+import './App.css';
+import { useState } from 'react';
+import Characters from './Components/Characters';
+
+function App() {
+  
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    // console.log("Clicking!");
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    // console.log("character Api: ", characterApi);
+    setCharacters(characterApi.results);
+  };
+
+  // console.log("characters: ", characters);
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
